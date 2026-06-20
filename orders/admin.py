327a6122b4d2f4ec5d5c922d8b085de7cdc3a1df
Priyanshu Tripathi <vibecoder144@gmail.com>
2026-06-20 14:56:46 +0530

    Admin Customization

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Order , RefundRequest , Product
+
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name' , 'price' , 'category' , 'in_stock']
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user' , 'product' , 'amount' , 'status' , 'carrier' , 'tracking_number']
 
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(RefundRequest)
+class RefundRequestAdmin(admin.ModelAdmin):
+    list_display = ['order' , 'user'  , 'reason' , 'status']
+
+admin.site.register(Product , ProductAdmin)
+admin.site.register(Order , OrderAdmin)
+admin.site.register(RefundRequest , RefundRequestAdmin)
