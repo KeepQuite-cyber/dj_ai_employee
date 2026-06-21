4c1b3237693e3675c319d0d34925524887971b82
Priyanshu Tripathi <vibecoder144@gmail.com>
2026-06-21 11:21:40 +0530

    Login Required Decorator and Logout feature implementeed

--- dj_ai_employee_main/urls.py
+++ dj_ai_employee_main/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
 
     # Login Endpoint
     path('login/' , auth_views.LoginView.as_view(template_name="login.html") , name="login"),
+    path('logout/' , auth_views.LogoutView.as_view() , name='logout'),
 
     path('orders/' , include('orders.urls')),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Order
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+@login_required
 def orders_list(request):
     orders = Order.objects.filter(user=request.user)
     context = {
